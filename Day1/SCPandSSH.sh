36aea497df590d51938ec13228c8ceb1e495810f
Rasika <rasikaprabhath@gmail.com>
2025-01-23 20:23:12 +0530

    2nd version with testing ping

--- Day1/SCPandSSH.sh
+++ Day1/SCPandSSH.sh
@@ -19,15 +19,39 @@ read -p "Enter 1 for  SCP and 2 for SSH : " input
 if [ "$input" == "1" ]
 then
     echo "SCP"
+    read -p "please Enter the IP Address: " ip
     read -p "Please Enter the User Name : " uname
     read -p "please Enter the Password  : " password
-    read -p "please Enter the IP Address: " ip
+    
+    if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]];
+    
+    then
+        IFS='.' read -r -a octets <<< "$ip"
+        
+        valid=true
+        for octet in "${octets[@]}"; # @ means all the elements in the array
+        do
+            if ((octet < 0 || octet > 255)); then
+                valid=false
+                break
+            fi
+        done
+
+        if [ "$valid" = true ]; then
+            echo "Valid IP address."
+        else
+            echo "Invalid IP address: Each octet should be between 0 and 255."
+        fi
+    else
+       echo "Invalid IP address format."
+    fi
+
 
     if [ -z "$uname" ] || [ -z "$password" ];
     then
-        echo "hi"
+        echo "User Name or Password cannot be Empty"
     else
-        echo "no"
+        echo "User Name And Passwrod are not empty"
     fi
 elif [ "$input" == "2" ]
 then
