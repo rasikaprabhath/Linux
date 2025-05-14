3353cb94a0780625cdd2a94c575794c1fdab2f78
rasikaprabhath <107238623+rasikaprabhath@users.noreply.github.com>
2025-05-15 11:09:12 +1200

    Update diskusage.sh

--- Day1/diskusage.sh
+++ Day1/diskusage.sh
@@ -2,7 +2,7 @@
 
 # Set the threshold percentage for disk usage
 THRESHOLD=95
-EMAIL="prabhath@biofoodslk.com"
+EMAIL="aaaa@sss.com"
 
 # Get disk usage for all /home-related directories
 df -h | grep '/home' | awk '{print $1, $5, $6}' | while read disk usage mount
